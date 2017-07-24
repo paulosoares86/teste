c51e187e8d5e29991935ce3b0abce7e04a0a2d0b
Paulo Soares <paulo_soares@mckinsey.com>
2017-07-24 17:14:46 -0500

    circle

--- mvn_prepare.sh
+++ mvn_prepare.sh
@@ -1,6 +1,1 @@
-if [[ `git log -1` =~ "[maven-release-plugin]" ]]; then
-    echo "Ignoring release plugin generated version"
-else
-    echo "Preparing release..."
-    mvn -B release:prepare
-fi
+
